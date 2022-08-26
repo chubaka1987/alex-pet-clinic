a749b73e35a9f703e06ab5e7961a6ca9595f00f7
Aleksandr Orshava <orshava@gmail.com>
2022-08-26 15:51:06 +0300

    Initial project add

---
+++ src/main/java/com/chubaka/alexpetclinic/AlexPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.chubaka.alexpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chubaka/alexpetclinic/AlexPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chubaka.alexpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
